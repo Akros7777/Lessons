4907354be2c3afa5b8a426a855e9835cda832b35
Artur Bichek <bichek.artur@yandex.ru>
2017-04-24 00:07:59 +0300

    Lesson17 completed

--- Lesson17/Lesson17/Program.cs
+++ Lesson17/Lesson17/Program.cs
@@ -6,10 +6,53 @@ using System.Threading.Tasks;
 
 namespace Lesson17
 {
+    class Geometricfigure
+    {
+        public int coordinatesX;
+        public int coordinatesY;
+        public int coordinatesZ;
+        public Geometricfigure(int X, int Y)
+        {
+            this.coordinatesX = X;
+            this.coordinatesY = Y;
+        }
+    }
+    class Triangle : Geometricfigure
+    {
+        public int Perimeter;
+        public Triangle(int X, int Y, int perimeter) : base(X, Y)
+        {
+            this.Perimeter = perimeter;
+        }
+
+        public void DrawTriangle()
+        {
+            Console.WriteLine("Рисуем треугольник");
+        }
+    }
+
+        class Circle : Geometricfigure
+        {
+            public int Radius;
+            public Circle(int X, int Y, int radius) : base(X, Y)
+            {
+                this.Radius = radius;
+            }
+            public void DrawCircle()
+            {
+                Console.WriteLine("Рисуем круг");
+            }
+        }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Triangle triangle1 = new Triangle(5, 8, 24);
+            triangle1.DrawTriangle();
+            Circle circle1 = new Circle(5, 7, 64);
+            circle1.DrawCircle();
+            Console.ReadKey();
         }
     }
 }
