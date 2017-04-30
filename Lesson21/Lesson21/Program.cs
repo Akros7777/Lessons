15ad3ee5bd128b07f35ba59d63c45b8f60c356e9
Artur Bichek <bichek.artur@yandex.ru>
2017-04-30 19:08:25 +0300

    Lesson21 completed

---
+++ Lesson21/Lesson21/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21
+{
+    abstract class Human
+    {
+        public string Name { get; set; }
+
+        public Human (string name)
+        {
+            Name = name;
+        }
+        public abstract void sayHello();
+    }
+
+    class Ukrain : Human
+    {
+        public string Nation { get; set; }
+
+        public Ukrain (string name, string nation): base (name)
+        {
+            Nation = nation;
+        }
+
+        public override void sayHello()
+        {
+            Console.WriteLine("{1} {0} говорит: Привiт!", Name, Nation);
+        }
+    }
+
+    class Russian : Human
+    {
+        public string Nation { get; set; }
+        public Russian(string name, string nation): base (name)
+        {
+            Nation = nation;
+        }
+
+        public override void sayHello()
+        {
+            Console.WriteLine("{1} {0} говорит: Привет!", Name, Nation);
+        }
+    }
+
+    class American : Human
+    {
+        public string Nation { get; set; }
+        public American(string name, string nation): base (name)
+        {
+            Nation = nation;
+        }
+
+        public override void sayHello()
+        {
+            Console.WriteLine("{1} {0} говорит: Hello!", Name, Nation);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ukrain ukrMan = new Ukrain("Иван", "Украинец");
+            ukrMan.sayHello();
+            Russian rusMan = new Russian("Антон", "Россиянин");
+            rusMan.sayHello();
+            American americMan = new American("Кирил", "Американец");
+            americMan.sayHello();
+            Console.ReadLine();
+        }
+    }
+}
