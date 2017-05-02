c09eac9a55e119e4748bd28bec30b8cb2381e48a
Artur Bichek <bichek.artur@yandex.ru>
2017-05-02 09:15:44 +0300

    Lesson22 completed

---
+++ Lesson22/Lesson22/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson22
+{
+    interface ISwitchable
+    {
+        void switchON();
+        void switchOFF();
+    }
+    class TV : ISwitchable
+    {
+        public void switchON()
+        {
+            Console.WriteLine("Включить TV!");
+        }
+        public void switchOFF()
+        {
+            Console.WriteLine("Выключить TV!");
+        }
+    }
+    class PC : ISwitchable
+    {
+        public void switchON()
+        {
+            Console.WriteLine("Включить PC!");
+        }
+        public void switchOFF()
+        {
+            Console.WriteLine("Выключить PC!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <ISwitchable> swit = new List<ISwitchable>();
+            swit.Add(new TV());
+            swit.Add(new PC());
+
+            foreach (ISwitchable s in swit)
+            {
+                s.switchON();
+                s.switchOFF();
+            }
+            Console.ReadKey();
+        }
+    }
+}
