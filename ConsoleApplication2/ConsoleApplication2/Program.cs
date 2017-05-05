73a89d95a8f60578bb01f2b249a85a707d65de85
Artur Bichek <bichek.artur@yandex.ru>
2017-05-05 20:34:09 +0300

    no message

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Imy = new List<string>();
+            Imy.Add("Андрей");
+            Imy.Add("Иван");
+            Imy.Add("Вася");
+            Imy.Add("Петя");
+            Imy.Add("Настя");
+            Imy.Add("Лена");
+            Imy_to_string_proc(Imy);
+            Imy_to_string_proc(Imy, ",");
+            Imy_to_string_proc(Imy, 9);
+            Console.ReadKey();
+        }
+        public static void Imy_to_string_proc(List<string> imy, int separator)
+        {
+            string stroka = "";
+            foreach (string imy_item in imy)
+            { stroka = stroka + imy_item + separator; }
+            Console.WriteLine(stroka);
+        }
+        public static void Imy_to_string_proc(List<string> imy, string separator)
+        {
+            string stroka = "";
+            foreach (string imy_item in imy)
+            { stroka = stroka + imy_item + separator; }
+            Console.WriteLine(stroka);
+        }
+        public static void Imy_to_string_proc(List<string> imy)
+        {
+            string stroka = "";
+            foreach (string imy_item in imy)
+            { stroka = stroka + imy_item + "[]"; }
+            Console.WriteLine(stroka);
+        }
+    }
+}
---
+++ Lesson23/Lesson23/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> name = new List<string> { "Андрей", "Вася", "Иван", "Настя", "Лена" };
+            PeopleList(name);
+            PeopleList(name, 5);
+            Console.ReadKey();
+        }
+        static void PeopleList(List<string> name)
+        {
+            string line = "";
+            foreach (string name_list in name)
+            {
+                line += name_list + ", ";
+            }
+            Console.WriteLine(line);
+        }
+        static void PeopleList(List<string> name, int numbers)
+        {
+            string line = "";
+
+            foreach (string name_list in name)
+            {
+                    line += name_list + numbers + ", ";
+            }
+            Console.WriteLine(line);
+        }
+    }
+}
